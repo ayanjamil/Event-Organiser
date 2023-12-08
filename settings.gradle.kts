db676be3d73aed15a35b7e02d831fb06b3b95d87
ayanjamil <ayanjamil00@gmail.com>
2023-12-08 19:41:19 +0530

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "edc_test01"
+include(":app")
