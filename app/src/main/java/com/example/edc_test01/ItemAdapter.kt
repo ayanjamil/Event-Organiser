71b0420ade973223e4d641b7a465679c0ab3f08e
ayanjamil <ayanjamil00@gmail.com>
2023-12-20 14:11:51 +0530

    cleaning

--- app/src/main/java/com/example/edc_test01/ItemAdapter.kt
+++ app/src/main/java/com/example/edc_test01/ItemAdapter.kt
@@ -1,36 +1,37 @@
 package com.example.edc_test01
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.example.edc_test01.databinding.ItemsRowBinding
 
 class EventAdapter(private val eventList: ArrayList<event_model>) :
     RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_row,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.event_layout,parent,false)
         return EventViewHolder(view)
+        // nothing much to do here it is what it is this is how you do it without viewbinding
     }
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val event = eventList[position]
         holder.heading.text = event.tv_heading
         holder.description.text = event.tv_descrpition
         holder.image.setImageResource(event.image)
-
+        //assigning the positions of each event in the recyclerview
+        // the recycler views text = event which is a variale decleared locally which holds
+        // the values of a instance of eventList which is passed in the recyclerview
     }
-
     override fun getItemCount(): Int {
         return eventList.size
     }
-
     class EventViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
         val image:ImageView = itemView.findViewById(R.id.image)
         val heading:TextView =itemView.findViewById(R.id.headding_event01)
         val description:TextView =itemView.findViewById(R.id.tv_loctimedetails)
+        //importing all the ids of the itemrow XML as that XML is an example of how each element
+        // in the recyclerview should be
 
     }
+    //can also use binding in the above block
 }
--- app/src/main/java/com/example/edc_test01/event_model.kt
+++ app/src/main/java/com/example/edc_test01/event_model.kt
@@ -1,5 +1,6 @@
 package com.example.edc_test01
 
-
+//making a class for passing a specific type of data into the recyclerview which is
+// haveing a ImageView and two TextViews
 
 data class event_model(var image:Int,var tv_heading:String,var tv_descrpition:String)
