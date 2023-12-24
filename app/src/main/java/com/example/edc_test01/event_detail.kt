4968aa90c553c18a3a04050ef67adf04862747a2
ayanjamil <ayanjamil00@gmail.com>
2023-12-25 01:45:38 +0530

    talk to speach

--- app/src/main/java/com/example/edc_test01/event_detail.kt
+++ app/src/main/java/com/example/edc_test01/event_detail.kt
@@ -1,13 +1,14 @@
 package com.example.edc_test01
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -15,8 +16,10 @@ import java.util.Date
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class event_detail : AppCompatActivity() {
+
+class event_detail : AppCompatActivity(),TextToSpeech.OnInitListener {
     private var restTimer: CountDownTimer? = null
+    private var tts: TextToSpeech? = null // Variable for TextToSpeech
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,7 @@ class event_detail : AppCompatActivity() {
             val descriptionContent:TextView=findViewById(R.id.tv_description_content)
             val location:TextView=findViewById(R.id.tv_location)
             val time:TextView=findViewById(R.id.tv_time)
+            val registerBtn = findViewById<Button>(R.id.btn_register)
             //above part is linking event_details kind of imporitng its ids
             imageView.setImageResource(event.eventImage)
             eventName.text=event.eventName
@@ -39,11 +43,15 @@ class event_detail : AppCompatActivity() {
             time.text="Time & Date:$eventTime"
             setDate(eventTime)
             //assigning values to the variables of event_detail activity by importing
-        // information of class and passing it here
+            // information of class and passing it here
+            //text to speach part
+            tts = TextToSpeech(this,this)
+                val event_name = event.eventName
+                speakOut(event_name) // it can only hold strings
+                Log.d("event_name issue","the event name is $event_name")
         }
         else
             Log.d("event_details issue","when the if block is not executed in event_datials secton")
-
     }
     private fun setDate(eventTime:String){
         val displaytimer = findViewById<TextView>(R.id.tv_countdown)
@@ -90,5 +98,39 @@ class event_detail : AppCompatActivity() {
             restTimer?.start()
         }
         // Countdown
+    }
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+override fun onInit(status: Int) {
+    if (status == TextToSpeech.SUCCESS) {
+        val result = tts!!.setLanguage(Locale.US)
+
+        if (result != TextToSpeech.LANG_MISSING_DATA && result != TextToSpeech.LANG_NOT_SUPPORTED) {
+            // Retrieve the event
+            val event = intent.getParcelableExtra("event",event_model::class.java)
+            if (event != null) {
+                // Speak out the event name
+                val eventName = event.eventName
+                speakOut(eventName.toString())
+            }
+            //needed to do all this just because we wanted the event name to speak up automatically without pressing any buttons
+        } else {
+            Log.e("TTS", "The Language specified is not supported!")
+        }
+    } else {
+        Log.e("TTS", "Initialization Failed!")
+    }
+}
+    public override fun onDestroy() {
+        if (tts != null) {
+            tts?.stop()
+            tts?.shutdown()
+        }
+        super.onDestroy()
+    }
+    private fun speakOut(text: String) {
+        Toast.makeText(this@event_detail, "Welcome to $text", Toast.LENGTH_SHORT)
+            .show()
+        Log.d("tts_init","Text to speach block initiated $text")
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
